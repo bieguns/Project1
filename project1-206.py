200ef147e24799bbfa1467d65c1519236476cdcb
Sam Biegun <bieguns@umich.edu>
2018-10-23 22:21:25 -0400

    Worked on functions.

--- project1-206.py
+++ project1-206.py
@@ -9,15 +9,26 @@ def getData(file):
 #Input: file name
 #Ouput: return a list of dictionary objects where
 #the keys are from the first row in the data. and the values are each of the other rows
+	data = []
 
-	pass
+	file_data = open(file,'r')
+	header_line = file_data.readline()
+	line = file_data.readline()
+	while line:
+		line_dict = {}
+		for col in range(len(header_line.strip().split(','))):
+			line_dict[header_line.strip().split(',')[col]] = line.strip().split(',')[col]
+		data.append(line_dict)
+		line = file_data.readline()
+	file_data.close()
+	return data
 
 def mySort(data,col):
 # Sort based on key/column
 #Input: list of dictionaries and col (key) to sort on
 #Output: Return the first item in the sorted list as a string of just: firstName lastName
-
-	pass
+	sorted_data = sorted(data, key = lambda line: line[col])
+	return(sorted_data[0]['First']+' '+sorted_data[0]['Last'])
 
 
 def classSizes(data):
@@ -26,16 +37,37 @@ def classSizes(data):
 # Output: Return a list of tuples sorted by the number of students in that class in
 # descending order
 # [('Senior', 26), ('Junior', 25), ('Freshman', 21), ('Sophomore', 18)]
+	numFreshman = 0
+	numSophmores = 0
+	numJuniors = 0
+	numSeniors = 0
 
-	pass
+	for line in data:
+		classType = line['Class']
+		if classType == 'Freshman':
+			numFreshman += 1
+		elif classType == 'Sophomore':
+			numSophmores += 1
+		elif classType == 'Junior':
+			numJuniors += 1
+		elif classType == 'Senior':
+			numSeniors += 1
+	result = sorted([('Freshman',numFreshman),('Sophomore',numSophmores),('Junior',numJuniors),('Senior',numSeniors)], key=lambda x:x[1], reverse=True)
+	return result
 
 
 def findMonth(a):
 # Find the most common birth month form this data
 # Input: list of dictionaries
 # Output: Return the month (1-12) that had the most births in the data
-
-	pass
+	monthFreq = {}
+	for line in a:
+		bDayMonth = int(line['DOB'].split('/')[0])
+		if bDayMonth in monthFreq:
+			monthFreq[bDayMonth] += 1
+		else:
+			monthFreq[bDayMonth] = 1
+	return sorted(monthFreq.keys(), key=lambda x: monthFreq[x], reverse=True)[0]
 
 def mySortPrint(a,col,fileName):
 #Similar to mySort, but instead of returning single
@@ -43,8 +75,11 @@ def mySortPrint(a,col,fileName):
 # as fist,last,email
 #Input: list of dictionaries, col (key) to sort by and output file name
 #Output: No return value, but the file is written
-
-	pass
+	sorted_data = sorted(a, key = lambda line: line[col])
+	file = open(fileName,'w')
+	for line in sorted_data:
+		file.write(line['First']+','+line['Last']+','+line['Email']+'\n')
+	file.close()
 
 def findAge(a):
 # def findAge(a):
@@ -52,8 +87,11 @@ def findAge(a):
 # Output: Return the average age of the students and round that age to the nearest
 # integer.  You will need to work with the DOB and the current date to find the current
 # age in years.
+	ageSum = 0
 
-	pass
+	for line in a:
+		ageSum += 2018 - int(line['DOB'].split('/')[2])
+	return round(ageSum/len(a))
 
 
 ################################################################
